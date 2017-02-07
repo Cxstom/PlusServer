1d8e0296e559da2d69defece3d4834cb9512c64f
Westyy <bradwestgarth@gmail.com>
2017-02-07 21:05:20 +0000

    Removed HC notification.

--- Communication/Packets/Incoming/Inventory/Purse/GetHabboClubWindowEvent.cs
+++ Communication/Packets/Incoming/Inventory/Purse/GetHabboClubWindowEvent.cs
@@ -9,7 +9,7 @@ namespace Plus.Communication.Packets.Incoming.Inventory.Purse
     {
         public void Parse(HabboHotel.GameClients.GameClient Session, ClientPacket Packet)
         {
-            Session.SendNotification("Habbo Club is free for all members, enjoy!");
+           // Session.SendNotification("Habbo Club is free for all members, enjoy!");
         }
     }
 }
