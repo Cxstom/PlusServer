518298aacfd41408f29f52ce2cee8f56f4a0d26f
Craig Sledmore <sledmore2012@gmail.com>
2017-02-26 23:52:14 +0000

    Refactoring, removed SurveyManager.

--- HabboHotel/Rewards/Reward.cs
+++ HabboHotel/Rewards/Reward.cs
@@ -9,16 +9,17 @@ namespace Plus.HabboHotel.Rewards
         public RewardType Type { get; set; }
         public string RewardData { get; set; }
         public string Message { get; set; }
-        public Reward(double Start, double End, string Type, string RewardData, string Message)
+
+        public Reward(double start, double end, string type, string rewardData, string message)
         {
-            this.RewardStart = Start;
-            this.RewardEnd = End;
-            this.Type = RewardTypeUtility.GetType(Type);
-            this.RewardData = RewardData;
-            this.Message = Message;
+            this.RewardStart = start;
+            this.RewardEnd = end;
+            this.Type = RewardTypeUtility.GetType(type);
+            this.RewardData = rewardData;
+            this.Message = message;
         }
 
-        public bool isActive()
+        public bool IsActive()
         {
             double Now = UnixTimestamp.GetNow();
             return (Now >= RewardStart && Now <= RewardEnd);
