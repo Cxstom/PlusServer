629e18988530a8a648b904e459c43dc167d7d32d
Westyy <bradwestgarth@gmail.com>
2017-03-03 18:10:07 +0000

    fixed.

--- Communication/Packets/Incoming/Rooms/Furni/RentableSpaces/BuyRentableSpaceEvent.cs
+++ Communication/Packets/Incoming/Rooms/Furni/RentableSpaces/BuyRentableSpaceEvent.cs
@@ -24,22 +24,33 @@ namespace Plus.Communication.Packets.Incoming.Rooms.Furni.RentableSpaces
             if (room == null || room.GetRoomItemHandler() == null)
                 return;
 
+            Item item = room.GetRoomItemHandler().GetItem(itemId);
+            if (item == null)
                 return;
 
+            RentableSpaceItem _rentableSpace;
+            if (!PlusEnvironment.GetGame().GetRentableSpaceManager().GetRentableSpaceItem(itemId, out _rentableSpace))
                 return;
 
+            int errorCode = PlusEnvironment.GetGame().GetRentableSpaceManager().GetBuyErrorCode(Session, _rentableSpace);
             if (errorCode > 0)
             {
+                Session.SendPacket(new RentableSpaceComposer(_rentableSpace.Rented, errorCode, _rentableSpace.OwnerId, _rentableSpace.OwnerUsername, _rentableSpace.GetExpireSeconds(), _rentableSpace.Price));
             }
             else
             {
+                if (PlusEnvironment.GetGame().GetRentableSpaceManager().ConfirmBuy(Session, _rentableSpace, 604800))
                 {
                     
+                    Session.SendPacket(new RentableSpaceComposer(_rentableSpace.OwnerId, _rentableSpace.OwnerUsername, _rentableSpace.GetExpireSeconds()));
+                    item.ExtraData = "2";
+                    item.UpdateState(false, true);
 
                 }
                 else
                 {
                     
+                    Session.SendPacket(new RentableSpaceComposer(_rentableSpace.Rented, 400, _rentableSpace.OwnerId, _rentableSpace.OwnerUsername, _rentableSpace.GetExpireSeconds(), _rentableSpace.Price));
                 }
             }
 
